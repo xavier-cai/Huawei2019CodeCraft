d7bda42a397d115d02645d9c0c9b2f9226e0ef1d
XavierCai <cxw39@foxmail.com>
2019-05-01 16:52:57 +0800

    Update CodeCraft-2019.cpp

--- CodeCraft2019/CodeCraft-2019.cpp
+++ CodeCraft2019/CodeCraft-2019.cpp
@@ -75,9 +75,6 @@ private:
 public:
     int Run(int argc, char *argv[])
     {
-        Random rngJitter;
-        rngJitter.SetSeedAutoImpl();
-
         //Log::Default(Log::ENABLE);
         Log::Enable<Scenario>();
         Log::Enable<SchedulerAnswer>();
@@ -89,96 +86,38 @@ public:
         Log::Enable<SchedulerTimeWeight>();
         //Log::Disable<LoadState>();
 
-        //char* set1[] = { "", "./config/car.txt", "./config/road.txt", "./config/cross.txt", "./config/answer.txt" };
-        char* set3[] = { "", "./config3-exam-1/car.txt", "./config3-exam-1/road.txt", "./config3-exam-1/cross.txt", "./config3-exam-1/presetAnswer.txt", "./config3-exam-1/answer.txt" };
-        char* set4[] = { "", "./config3-exam-2/car.txt", "./config3-exam-2/road.txt", "./config3-exam-2/cross.txt", "./config3-exam-2/presetAnswer.txt", "./config3-exam-2/answer.txt" };
         char* set1[] = { "", "./config2-1/car.txt", "./config2-1/road.txt", "./config2-1/cross.txt", "./config2-1/presetAnswer.txt", "./config2-1/answer.txt" };
         char* set2[] = { "", "./config2-2/car.txt", "./config2-2/road.txt", "./config2-2/cross.txt", "./config2-2/presetAnswer.txt", "./config2-2/answer.txt" };
-        char* set5[] = { "", "./config3-1/car.txt", "./config3-1/road.txt", "./config3-1/cross.txt", "./config3-1/presetAnswer.txt", "./config3-1/answer.txt" };
-        char* set6[] = { "", "./config3-2/car.txt", "./config3-2/road.txt", "./config3-2/cross.txt", "./config3-2/presetAnswer.txt", "./config3-2/answer.txt" };
+        char* set3[] = { "", "./config3-1/car.txt", "./config3-1/road.txt", "./config3-1/cross.txt", "./config3-1/presetAnswer.txt", "./config3-1/answer.txt" };
+        char* set4[] = { "", "./config3-2/car.txt", "./config3-2/road.txt", "./config3-2/cross.txt", "./config3-2/presetAnswer.txt", "./config3-2/answer.txt" };
+        char* set5[] = { "", "./config3-exam-1/car.txt", "./config3-exam-1/road.txt", "./config3-exam-1/cross.txt", "./config3-exam-1/presetAnswer.txt", "./config3-exam-1/answer.txt" };
+        char* set6[] = { "", "./config3-exam-2/car.txt", "./config3-exam-2/road.txt", "./config3-exam-2/cross.txt", "./config3-exam-2/presetAnswer.txt", "./config3-exam-2/answer.txt" };
+        
+        argc = 6;
         argv = set1;
-        if (false)
-        {
-            int bestTime = -1;
-            int bestArg1 = -1, bestArg2 = -1;
-            for (int iArg1 = 140; iArg1 <= 230; iArg1 += 80)
-            {
-                for (int iArg2 = 270; iArg2 <= 320; iArg2 += 30)
-                {
-                    for (int sample = 0; sample < 1; sample++)
-                    {
-                        if (iArg1 != 140 || iArg2 != 300)
-                            continue;
-                        int arg1 = iArg1;
-                        int arg2 = iArg2;
-                        if(sample != 0)
-                        {
-                            arg1 += rngJitter.NextUniform(0, 5) - 2;
-                            arg2 += rngJitter.NextUniform(0, 5) - 2;
-                        }
-                        //SchedulerFloyd::lenthWeight = arg1 / 100.0;
-                        //SchedulerFloyd::carLimit = arg2 / 100.0;
-                        bool success = true;
-                        int total = 0;
-                        for (int i = 1; i <= 2; ++i)
-                        {
-                            //SchedulerFloyd scheduler;
-                            SchedulerAnswer scheduler;
-                            int cost = RunImpl(6, i == 1 ? set1 : set2, &scheduler, false);
-                            if (cost <= 0)
-                                success = false;
-                            else
-                                total += cost;
-                            LOG(arg1 << " " << arg2 << " : " << cost);
-                        }
-                        if (success)
-                        {
-                            if (bestTime < 0 || total < bestTime)
-                            {
-                                bestTime = total;
-                                bestArg1 = arg1;
-                                bestArg2 = arg2;
-                            }
-                        }
-                    }
-                }
-            }
-            LOG("best : " << bestArg1 << " " << bestArg2 << " : " << bestTime);
-        }
-        else
-        {
-            for (int i = 1; i <= 1; ++i)
-            {
-                //Log::Enable<RunFramework>();
-                //RunFramework framework;
-                //framework.Run(6, i == 1 ? set1 : set2);
-                //return 0;
-                //SchedulerFloyd::lenthWeight = int(6) / 10.0;
-                //SchedulerFloyd::lanesWeight = int(20) / 100.0;
-                //SchedulerFloyd::carLimit = int(28) / 10.0;
-                /*
-                SchedulerFloyd scheduler;
-                scheduler.SetLengthWeight(1.6);
-                scheduler.SetIsDropBackByDijkstra(false);
-                scheduler.SetIsEnableVipWeight(true);
-                scheduler.SetIsFasterAtEndStep(true);
-                scheduler.SetIsLessCarAfterDeadLock(false);
-                scheduler.SetIsLimitedByRoadSizeCount(true);
-                scheduler.SetIsOptimalForLastVipCar(true);
-                scheduler.SetIsVipCarDispatchFree(false);
-                scheduler.SetPresetVipTracePreloadWeight(0.3);
-                */
-                SchedulerTimeWeight scheduler;
-                //SchedulerAnswer scheduler;
 
-                scheduler.EnableTrace("log.tr");
-                int ret = RunImpl(6, i == 1 ? set3 : set4, &scheduler, true);
-                LOG(ret);
-            }
-        }
+        //Log::Enable<RunFramework>();
+        //RunFramework framework;
+        //framework.Run(argc, argv);
 
-        //SchedulerAnswer schedulerAnswer;
-        //ASSERT(RunImpl(argc, argv, &schedulerAnswer, false) >= 0);
+        SchedulerFloyd scheduler;
+        scheduler.SetLengthWeight(1.6);
+        scheduler.SetIsDropBackByDijkstra(false);
+        scheduler.SetIsEnableVipWeight(true);
+        scheduler.SetIsFasterAtEndStep(true);
+        scheduler.SetIsLessCarAfterDeadLock(false);
+        scheduler.SetIsLimitedByRoadSizeCount(true);
+        scheduler.SetIsOptimalForLastVipCar(true);
+        scheduler.SetIsVipCarDispatchFree(false);
+        scheduler.SetPresetVipTracePreloadWeight(0.3);
+
+        //SchedulerTimeWeight scheduler;
+        //SchedulerAnswer scheduler;
+
+        scheduler.EnableTrace("log.tr");
+        int ret = RunImpl(argc, argv, &scheduler, true);
+        LOG(ret);
+
         Timer::Print();
         return 0;
     }
